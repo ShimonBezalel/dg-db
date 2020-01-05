147f8f07691e754de5467347fa59f28914007d52
shimonheimowitz <heimy4prez@gmail.com>
2020-01-05 14:19:53 +0200

    First commit - setup for flavor db api

---
+++ parse_fooddb.py
@@ -0,0 +1,17 @@
+
+
+API_URL = "https://cosylab.iiitd.edu.in/flavordb/entities_json"
+
+PARAM_ID = "id"
+
+
+def get_entity(flavordb_id):
+    """
+    Retrieves the data for a given entity by ID. The data is returned in json format.
+    :param flavordb_id:
+    :return:
+    """
+    pass
+
+def parse_entity(data):
+    pass
